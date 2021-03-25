2ed3e764bd3b91bc2b225c4721696754125b4416
fatimafp95 <53170817+fatimafp95@users.noreply.github.com>
2021-03-25 12:47:17 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 export GOOS=linux
 export VOLANTMQ_WORK_DIR=/usr/lib/volantmq
 export VOLANTMQ_BUILD_FLAGS="-i"
@@ -11,18 +10,28 @@ sudo mkdir -p $VOLANTMQ_WORK_DIR/bin
 sudo mkdir -p $VOLANTMQ_WORK_DIR/conf
 sudo mkdir -p $VOLANTMQ_PLUGINS_DIR
 
-#Compilar servidor VOLANT
-
+#Build the broker
+export GO111MODULE=off
+go get github.com/fatimafp95/quic-go
+cd $GOPATH/src/github.com/fatimafp95/quic-go
+export GO111MODULE=on
+go mod tidy
+export GO111MODULE=off
+go get github.com/fatimafp95/quic-go
+cd $GOPATH/src/github.com/fatimafp95/volantmq_2
+export GO111MODULE=on
+go mod tidy
 sudo cp $GOPATH/src/github.com/fatimafp95/volantmq_2/tools/print_version.sh /bin
 export GO111MODULE=off
 go get github.com/troian/govvv
 cd cmd/volantmq/
-sudo /usr/local/go/bin/go build $VOLANTMQ_BUILD_FLAGS -o $VOLANTMQ_WORK_DIR/bin/volantmq
+sudo /usr/local/go/bin/go build $VOLANTMQ_BUILD_FLAGS -o volantmq
 sudo cp volantmq $VOLANTMQ_WORK_DIR/bin/volantmq
 
-#Compilar debug plugins
+#Debug plugin
 go get gitlab.com/VolantMQ/vlplugin/debug
 cd $GOPATH/src/gitlab.com/VolantMQ/vlplugin/debug
+git checkout tags/v0.0.8
 export GO111MODULE=on
 go mod tidy
 sudo /usr/local/go/bin/go build $VOLANTMQ_BUILD_FLAGS -buildmode=plugin -ldflags "-X main.version=$(print_version.sh)" -o $VOLANTMQ_WORK_DIR/plugins/debug.so
@@ -30,13 +39,17 @@ sudo /usr/local/go/bin/go build $VOLANTMQ_BUILD_FLAGS -buildmode=plugin -ldflags
 #Health plugin
 export GO111MODULE=off
 go get gitlab.com/VolantMQ/vlplugin/health
+cd $GOPATH/src/gitlab.com/VolantMQ/vlplugin/health
+git checkout tags/v0.0.8
 export GO111MODULE=on
+go mod tidy
 sudo /usr/local/go/bin/go build $VOLANTMQ_BUILD_FLAGS -buildmode=plugin -ldflags "-X main.version=$(print_version.sh)" -o $VOLANTMQ_WORK_DIR/plugins/health.so
 
-#build metrics plugins 
+#Metrics plugins 
 export GO111MODULE=off
 go get gitlab.com/VolantMQ/vlplugin/monitoring/prometheus
 cd $GOPATH/src/gitlab.com/VolantMQ/vlplugin/monitoring/prometheus
+git checkout tags/v0.0.5
 export GO111MODULE=on
 go mod tidy
 sudo /usr/local/go/bin/go build $VOLANTMQ_BUILD_FLAGS -buildmode=plugin -ldflags "-X main.version=$(print_version.sh)" -o $VOLANTMQ_WORK_DIR/plugins/prometheus.so
@@ -44,23 +57,26 @@ sudo /usr/local/go/bin/go build $VOLANTMQ_BUILD_FLAGS -buildmode=plugin -ldflags
 export GO111MODULE=off
 go get gitlab.com/VolantMQ/vlplugin/monitoring/systree
 cd $GOPATH/src/gitlab.com/VolantMQ/vlplugin/monitoring/systree
+git checkout tags/v0.0.7
 export GO111MODULE=on
 go mod tidy
 sudo /usr/local/go/bin/go build $VOLANTMQ_BUILD_FLAGS -buildmode=plugin -ldflags "-X main.version=$(print_version.sh)" -o $VOLANTMQ_WORK_DIR/plugins/systree.so
 
-#build persistence plugins
+#Persistence plugins
 export GO111MODULE=off
 go get gitlab.com/VolantMQ/vlplugin/persistence/bbolt
 cd $GOPATH/src/gitlab.com/VolantMQ/vlplugin/persistence/bbolt
+git checkout tags/v0.0.7
 export GO111MODULE=on
 go mod tidy
 cd plugin
 sudo /usr/local/go/bin/go build $VOLANTMQ_BUILD_FLAGS -buildmode=plugin -ldflags "-X main.version=$(print_version.sh)" -o $VOLANTMQ_WORK_DIR/plugins/persistence_bbolt.so
 
-#build auth plugins
+#Auth plugins
 export GO111MODULE=off
 go get gitlab.com/VolantMQ/vlplugin/auth/http
 cd $GOPATH/src/gitlab.com/VolantMQ/vlplugin/auth/http
+git checkout tags/v0.0.2
 GO111MODULE=on
 go mod tidy
 sudo /usr/local/go/bin/go build $VOLANTMQ_BUILD_FLAGS -buildmode=plugin -ldflags "-X main.version=$(print_version.sh)" -o $VOLANTMQ_WORK_DIR/plugins/auth_http.so
@@ -68,4 +84,6 @@ sudo /usr/local/go/bin/go build $VOLANTMQ_BUILD_FLAGS -buildmode=plugin -ldflags
 #Running the broker with config.yaml file
 cd $GOPATH/src/github.com/fatimafp95/volantmq_2
 sudo cp -r /usr/lib/volantmq /var/lib
-sudo /usr/lib/volantmq/bin/volantmq --config=examples/config.yaml
+#sudo /usr/lib/volantmq/bin/volantmq --config=examples/config.yaml
+cd cmd/volantmq
+sudo ./volantmq --config=../../examples/config.yaml
